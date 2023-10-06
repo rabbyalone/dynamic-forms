a1467819cfed2d1d158cdac7975de269ae03cffd
Rabby Hasan <rabbyalone@gmail.com>
2023-10-07 00:46:04 +0600

    added dynamic forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'dynamic-forms';
+
+  form!: FormGroup;
+
+  medicineOptions = [
+    { id: 1, name: 'Medicine 1' },
+    { id: 2, name: 'Medicine 2' },
+    { id: 3, name: 'Medicine 3' }
+  ];
+
+  formGroupArray!: FormArray;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      medicineArr: this.fb.array([this.createForm()])
+    });
+
+    this.formGroupArray = this.form.get('medicineArr') as FormArray;
+  }
+
+  get medicineFormGroup() {
+    return this.form.get('medicineArr') as FormArray;
+  }
+
+  createForm(){
+    return this.fb.group({
+      medicine: [null],
+      amount: [''],
+      dosage: [null],
+      duration: [null]
+    });
+  }
+
+  addFormGroup() {
+    const formGroup = this.fb.group({
+      medicine: [''],
+      amount: [''],
+      dosage: [''],
+      duration: ['']
+    });
+
+    this.formGroupArray.push(formGroup as never);
+    this.form.addControl('medicineArr', formGroup);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
